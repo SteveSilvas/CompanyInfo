d2c002d0aa042bb286bd564252a4ba72bb1734e0
Steve Silva <Steve.Silva@copart.com>
2024-04-12 17:22:59 -0300

    Enriquecendo EmpresaConfiguration

--- API/Configurations/EmpresaConfiguration.cs
+++ API/Configurations/EmpresaConfiguration.cs
@@ -16,41 +16,130 @@ namespace CompanyInfo.Configurations
                 .HasColumnName("Id")
                 .ValueGeneratedOnAdd();
 
-            builder.Property(a => a.CNPJ)
-                .HasColumnName("CNPJ")
-                .IsRequired();
-
             builder.Property(a => a.Abertura)
                 .HasColumnName("Abertura")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Situacao)
+                .HasColumnName("Situacao")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Tipo)
+                .HasColumnName("Tipo")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Nome)
+                .HasColumnName("Nome")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Fantasia)
+                .HasColumnName("Fantasia")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Porte)
+                .HasColumnName("Porte")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.NaturezaJuridica)
+                .HasColumnName("NaturezaJuridica")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Logradouro)
+                .HasColumnName("Logradouro")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Numero)
+                .HasColumnName("Numero")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Complemento)
+                .HasColumnName("Complemento")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Municipio)
+                .HasColumnName("Municipio")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.Bairro)
+                .HasColumnName("Bairro")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.UF)
+                .HasColumnName("UF")
+                .HasColumnType("varchar(255)")
                 .IsRequired();
 
             builder.Property(a => a.CEP)
-             .HasColumnName("CEP")
-             .IsRequired();
+                .HasColumnName("CEP")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
 
-            builder.Property(a => a.Bairro)
-                .HasColumnName("Bairro");
+            builder.Property(a => a.Telefone)
+                .HasColumnName("Telefone")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.DataSituacao)
+                .HasColumnName("DataSituacao")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.CNPJ)
+                .HasColumnName("CNPJ")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.UltimaAtualizacao)
+                .HasColumnName("UltimaAtualizacao")
+                .HasColumnType("datetime")
+                .IsRequired();
 
             builder.Property(a => a.Status)
-                .HasColumnName("Status");
+                .HasColumnName("Status")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
 
-            //builder.Property(a => a.AtividadePrincipal)
-            //    .HasColumnName("AtividadePrincipal");
+            builder.Property(a => a.Email)
+                .HasColumnName("Email")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
 
-            //builder.Property(a => a.AtividadesSecundarias)
-            //    .HasColumnName("AtividadesSecundarias")
-            //    .IsRequired();
+            builder.Property(a => a.EFR)
+                .HasColumnName("EFR")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.MotivoSituacao)
+                .HasColumnName("MotivoSituacao")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.SituacaoEspecial)
+                .HasColumnName("SituacaoEspecial")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(a => a.DataSituacaoEspecial)
+                .HasColumnName("DataSituacaoEspecial")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
 
             builder.Property(a => a.CapitalSocial)
-                .HasColumnName("CapitalSocial");
-
-            //builder.HasOne(a => a.Billing)
-            //    .WithMany()
-            //    .HasForeignKey(a => a.BillingId);
-
-            //builder.HasOne(a => a.Id)
-            //    .WithMany()
-            //    .HasForeignKey(a => a.StateId);
+                .HasColumnName("CapitalSocial")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
         }
     }
 }
